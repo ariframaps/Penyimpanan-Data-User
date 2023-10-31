e6b5adde7e2a0026c7cab6607f3c8466bcc25726
ariframaps <ariframa@student.ub.ac.id>
2023-10-31 12:25:42 +0700

    membuat alur kode javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+// mengambil data pada local web storage
+// membuat fungsi submit pada form
+// fungsi submit menyimpan data di lokal
+// jika data lokal lebih dari 5 maka data terlama dihapus
+// selesai menyimpan maka render data di user list
+
+renderData();
+
+document.addEventListener('DOMContentLoaded', function () {
+
+});
