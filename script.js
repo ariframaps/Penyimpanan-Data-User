2b0d7c027dce50fc5f67e92a35eb237432e10616
ariframaps <ariframa@student.ub.ac.id>
2023-10-31 17:04:33 +0700

    membuat event listener pada submit form

--- script.js
+++ script.js
@@ -5,7 +5,24 @@
 // selesai menyimpan maka render data di user list
 
 renderData();
+const form = document.getElementById('form');
+console.log(form);
 
-document.addEventListener('DOMContentLoaded', function () {
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
+    const namaUser = document.getElementById('inputNama').value;
+    const umurUser = document.getElementById('inputUmur').value;
+    const domisiliUser = document.getElementById('inputDomisili').value;
+    submitData(namaUser, umurUser, domisiliUser);
+
+    renderData();
 });
+
+function submitData(namaUser, umurUser, domisiliUser) {
+
+}
+
+function renderData() {
+
+}
